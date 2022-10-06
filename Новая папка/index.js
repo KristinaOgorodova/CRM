23fa72ccca91ec350dc75d745780465c982d726f
ogorodova.kristina <ogorodova.kristina@DRBRU.mst>
2022-10-06 09:39:07 +0300

    delete errors

--- Новая папка/index.js
+++ Новая папка/index.js
@@ -1,19 +1,11 @@
 'use strict'
 
-const item = "картина";
-const itemCount = 2;
-const itemCategory = "украшения";
-const itemPrice = 2500;
 
-console.log("Наименование:", item);
-console.log(itemCount*itemPrice);
+const itemName = prompt("Наименование товара", "картина");
+const countOfItem = Number(prompt("Количество товара", 2));
+const Category = prompt("Категория товара", "украшения");
+const price =  Number(prompt("Цена товара", 2500));
 
-const itemName = prompt("Наименование товара");
-const count = prompt("Количество товара");
-const countOfItem = Number(count);
-const Category = prompt("Категория товара");
-const num =  prompt("Цена товара");
-const price = Number(num);
 
 console.log(typeof itemName);
 console.log(typeof countOfItem);
