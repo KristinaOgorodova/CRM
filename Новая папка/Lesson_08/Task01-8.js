23020f255de87a1128482a37d36b7cb2d80564ba
ogorodova.kristina <ogorodova.kristina@DRBRU.mst>
2022-10-11 15:47:52 +0300

    lesson8

--- Новая папка/Lesson_08/Task01-8.js
+++ Новая папка/Lesson_08/Task01-8.js
@@ -2,14 +2,11 @@
 
 const rendomNumbers = (num) => {
     let arr = [];
-    arr.length = num;
-    let rand = Math.round(0 - 0.5 + Math.random()* (100 - 0 + 1));
-    console.log(rand)
-    let newArr = arr.map((currentValue) => {
-        Math.round(0 - 0.5 + Math.random()* (100 - 0 + 1));
-    });
 
-    return newArr;
+    for (let i = 0; i < num; i++) {
+        arr.push(Math.round(0 - 0.5 + Math.random()* (100 - 0 + 1)))
+    }
+    return arr;
 }
 
-console.log(rendomNumbers(5));
+console.log(rendomNumbers(10));
