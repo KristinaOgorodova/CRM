34358da90174f89d2aad9f3bfdf69693fe4462c7
Kristina Ogorodova <skrok@inbox.ru>
2022-10-18 21:31:14 +0300

    eslint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
-const item = "картина";
+const item = 'картина';
 const itemCount = 2;
-const itemCategory = "украшения";
+const itemCategory = 'украшения';
 const itemPrice = 2500;
 
-console.log("Наименование:", item);
-console.log(itemCount*itemPrice);
+console.log('Наименование:', item);
+console.log(itemCount * itemPrice);
--- Новая папка/lesson_09/cart.js
+++ Новая папка/lesson_09/cart.js
@@ -1,42 +1,41 @@
-'use strict'
+'use strict';
 
 const cart = {
-    items: [],
-    totalPrice: 0,
-    count: 0,
-    getTotalPrice() {
-     return this.totalPrice;
-    },
+  items: [],
+  totalPrice: 0,
+  count: 0,
+  getTotalPrice() {
+    return this.totalPrice;
+  },
 
-    add(name, price, count =1) {
-        const obj = {name, price,count};
-        cart.items.push(obj);
-        this.increaseCount(count);
-        this.calculateItemPrice(price, count);
-    },
-    increaseCount(count) {
-       this.count += count;
-    },
+  add(name, price, count = 1) {
+    const obj = {name, price, count};
+    cart.items.push(obj);
+    this.increaseCount(count);
+    this.calculateItemPrice(price, count);
+  },
+  increaseCount(count) {
+    this.count += count;
+  },
 
-    calculateItemPrice (price,count) {
-     return this.totalPrice += price * count;
-    },
+  calculateItemPrice(price, count) {
+    return this.totalPrice += price * count;
+  },
 
-    clear() {
-       this.totalPrice = 0;
-       this.count = 0;
-       this.items =[];
-    },
-    print() {
-        console.log(JSON.stringify(cart));
-       this.getTotalPrice();
-    },
+  clear() {
+    this.totalPrice = 0;
+    this.count = 0;
+    this.items = [];
+  },
+  print() {
+    console.log(JSON.stringify(cart));
+    this.getTotalPrice();
+  },
 };
 
 cart.add('cup', 100, 5);
 cart.add('cup', 300, 2);
 cart.add('pancakes', 50, 5);
 cart.print();
-
 
 
