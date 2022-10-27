d92fc66d4aee3bb451c0cfa43e374933639f39a3
Kristina Ogorodova <skrok@inbox.ru>
2022-10-27 22:06:48 +0300

    work

--- Game2/marblGame.js
+++ Game2/marblGame.js
@@ -1,63 +1,61 @@
 'use strict';
 
 (() => {
+  const getRandomEvenOrOddNumber = () => {
+    let str;
+    const ran = Math.round(Math.random());
+    if (ran === 0) {
+      return str = 'even';
+    } else {
+      return str = 'odd';
+    }
+  };
 
-    const getRandomEvenOrOddNumber = () => {
-        let str;
-        let ran = Math.round(Math.random());
-        if (ran  === 0 ) {
-            return str = 'even';
-        } else {
-            return str = 'odd';
-        }
+  const game = () => {
+    const result = {
+      player: 5,
+      bot: 5,
     };
 
-    const game = () => {
-        const result = {
-            player:5,
-            bot:5,
-        };
-        
-        return function start() {
-            const computer = getRandomEvenOrOddNumber();
-            let userAnswer = 0;
-            userAnswer = +prompt('Введите количество шариков от 1 до 5: ');
-            console.log(computer);
-            console.log(userAnswer);
-            if (userAnswer > result.player || userAnswer < 0) {
-                alert(`Введите другое число`);
-                return start();
-            }
-        
-            if (userAnswer % 2 === 0 || computer === "even") {
-                result.bot = result.bot + userAnswer;
-                result.player = result.player - userAnswer;
-                alert(`Результат: У Вас:${result.player} , у Бота: ${result.bot}`);
-            } else if (userAnswer % 2 !== 0 || computer === "odd") {
-                result.bot = result.bot - userAnswer;
-                result.player = result.player + userAnswer;
-                alert(`Результат: У Вас: ${result.player} , у Бота: ${result.bot}`);
-            }
+    return function start() {
+      const computer = getRandomEvenOrOddNumber();
+      let userAnswer = 0;
+      userAnswer = +prompt('Введите количество шариков от 1 до 5: ');
+      console.log(computer);
+      console.log(userAnswer);
+      if (userAnswer > result.player || userAnswer < 0) {
+        alert(`Введите другое число`);
+        return start();
+      }
 
-            let newGame = false;
-            if (result.bot <= 0) {
-                newGame = confirm(`Вы выиграли! Сыграем еще?`)
-            } else if (result.player <= 0) {
-                newGame = confirm(`Вы проиграли! Сыграем еще?`)
-            } else {
-                start ();
-            }
+      if (userAnswer % 2 === 0 || computer === 'even') {
+        result.bot += userAnswer;
+        result.player -= userAnswer;
+        alert(`Результат: У Вас:${result.player} , у Бота: ${result.bot}`);
+      } else if (userAnswer % 2 !== 0 || computer === 'odd') {
+        result.bot -= userAnswer;
+        result.player += userAnswer;
+        alert(`Результат: У Вас: ${result.player} , у Бота: ${result.bot}`);
+      }
 
-            if (!newGame) {
-                alert(`Игра окончена!У Вас: ${result.player} , у Бота: ${result.bot} `)
-                return;
-            } else {
-                start();
-            }
+      let newGame = false;
+      if (result.bot <= 0) {
+        newGame = confirm(`Вы выиграли! Сыграем еще?`);
+      } else if (result.player <= 0) {
+        newGame = confirm(`Вы проиграли! Сыграем еще?`);
+      } else {
+        start();
+      }
 
-        }
-    }
-    window.marbl = game;
+      if (!newGame) {
+        alert(`Игра окончена!У Вас: ${result.player} , у Бота: ${result.bot} `);
+        return;
+      } else {
+        start();
+      }
+    };
+  };
+  window.marbl = game;
 })();
 
 
